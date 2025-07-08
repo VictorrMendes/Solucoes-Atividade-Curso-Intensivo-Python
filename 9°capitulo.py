47a2d37716bdc8c6771a6a9e4e441c9d1b7af6bb
VictorrMendes <victo.mendes.souza@gmail.com>
2025-07-07 21:23:20 -0300

    Add:Adcionando atividades

--- 9°capitulo.py
+++ 9°capitulo.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 # EXERCICIO 9.1-Restaurante
 print("EXERCICIO 9.1-Restaurante")
 
@@ -52,8 +54,8 @@ restaurante_4.describle_restaurant()
 
 print("\n----------------Fim do exercicio----------------\n")
 
-# EXERCICIO 9.2 - Usuarios
-print("EXERCICIO 9.2 - Usuarios")
+# EXERCICIO 9.3 - Usuarios
+print("EXERCICIO 9.3 - Usuarios")
 
 class User():
     def __init__(self, first_name, last_name, age, pet):
@@ -61,6 +63,7 @@ class User():
         self.last_name = last_name
         self.age = age
         self.pet = pet
+        self.login_attempst = 0
         
     def describle_user(self):
         nome_completo = self.first_name + " " + self.last_name
@@ -70,6 +73,18 @@ class User():
         
     def greet_user(self):
         print(f"Olá {self.first_name.title()}, sejá bem vindo ao petshop")
+        
+    def read_login_user(self):
+        print(str(self.login_attempst))
+        
+    def increment_login_attempts(self):
+        self.login_attempst += 1    
+        self.read_login_user()
+        
+    def reset_login_attempts(self):
+        self.login_attempst = 0
+        self.read_login_user()
+    
         
 usuario = User("victor", "mendes", "20", "gato")
 
@@ -95,3 +110,95 @@ print("\n----------------Fim do exercicio----------------\n")
 # EXERCICIO 9.5 Tentativas de login
 print("EXERCICIO 9.5 Tentativas de login")
 
+usuario.read_login_user()
+usuario.increment_login_attempts()
+usuario.reset_login_attempts()
+usuario.increment_login_attempts()
+usuario.increment_login_attempts()
+usuario.increment_login_attempts()
+usuario.increment_login_attempts()
+usuario.increment_login_attempts()
+usuario.reset_login_attempts()
+
+print("\n----------------Fim do exercicio----------------\n")
+
+# EXERCICIO 9.6 - Sorveteria
+print("EXERCICIO 9.6 - Sorveteria")
+
+class IceCreamStand(Restaurante):
+    def __init__(self,restaurant_name, cuisile_type, estado,):
+        super().__init__(restaurant_name, cuisile_type, estado)
+        self.flavors = ["maracuja", "chocolate", "manga"]
+        
+    def show_flavors(self):
+        for sabor in self.flavors:
+            print(f"O restaurante {self.name} tem servetes de {sabor}")
+            
+sorveteria = IceCreamStand("Ice", "sorvetes", "aberto")
+
+sorveteria.describle_restaurant()
+sorveteria.open_restaurant()
+sorveteria.show_flavors()
+
+print("\n----------------Fim do exercicio----------------\n")
+
+# EXERCICIO 9.7 - Admin
+print("EXERCICIO 9.7 - Admin")
+
+class Admin(User):
+    def __init__(self,first_name, last_name, age, pet):
+        super().__init__(first_name, last_name, age, pet)
+        self.privileges = ["can add post", "can delete post", "can ban user"]
+    
+    def show_privileges(self):
+        print(f"O {self.first_name} está como administrador e tem os seguintes privilegios:")
+        
+        for privilegio in self.privileges:
+            print(f"- {privilegio}")
+            
+adm = Admin("Victor", "Mendes", "18", "gato")
+
+adm.describle_user()
+adm.show_privileges()
+
+print("\n----------------Fim do exercicio----------------\n")
+
+# EXERCICIO 9.13-Reescrevendo o programa com OrdenedDict
+print("EXERCICIO 9.13-Reescrevendo o programa com OrdenedDict")
+
+glossario2 = OrderedDict()
+
+glossario2["chapter_1"] = "numbers"
+glossario2["chapter_2"] = "dicnames_casesionario"
+glossario2["chapter_3"] = "list"
+glossario2["chapter_4"] = "for"
+glossario2["chapter_5"] = "if"
+glossario2["chapter_6"] = "else"
+glossario2["chapter_6_1"] = "dicionario"
+glossario2["chapter_6_2"] = "items()"
+glossario2["chapter_6_3"] = "key()"
+glossario2["chapter_6_4"] = "value()"
+
+for key, value in glossario2.items():
+    print(f"Eu aprendi no {key} sobre: {value}\n")
+
+print("\n----------------Fim do exercicio----------------\n")
+
+# EXERCICIO 9.14 - Dados
+print("EXERCICIO 9.14 - Dados")
+from random import randint
+
+
+class Die():
+    def __init__(self,sides=6):
+        self.sides = sides
+        
+    def roll_die(self):
+        numero_aleatorio = randint(1, self.sides)
+        print(f"O numero é {numero_aleatorio}")
+            
+luck_number = Die()
+
+luck_number.roll_die()
+
+print("\n----------------Fim do exercicio----------------\n")
